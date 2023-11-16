8118c33a13d1f04a9116e2df3c6cfdf1ceb9ba4b
limitlesslogger <anushapaul789@gmail.com>
2023-11-16 18:51:32 +0530

    sll

--- sll-menu-driven.c
+++ sll-menu-driven.c
@@ -124,183 +124,128 @@ void insertBefore()
     }
     printf("No such element\n");
 }
-// void insertPosition()
-// {
-//     int pos=0,cnt=0;
-//     int flag=0;
-//     nd* prev;
-//     printf("Enter the position at which to insert\n");
-//     scanf("%d",&pos);
-//     nd* new_node=(nd*)malloc(sizeof(nd));
-//     printf("Enter new node data\n");
-//     scanf("%d",&new_node->data);
-//     nd* temp=head;
-//     if(head==NULL||pos==1)
-//     {
-//         new_node->next=head;
-//         head=new_node;
-//         flag=1;
-//         // return;
-//     }
-//     else 
-//     {
-//         while(temp!=NULL)
-//         {
-//             cnt++;
-//             if(pos!=cnt)
-//             {
-//                 prev=temp;
-//                 temp=temp->next;
-//             }
-//             else if(pos==cnt)
-//             {
-//                 new_node->next=temp;
-//                 prev->next=new_node;
-//                 flag=1;
-//             }
-//         }
-//         if(flag!=1&&prev!=NULL&&temp==NULL)
-//         {
-//             new_node->next=NULL;
-//             prev->next=new_node;
-//             flag=1;
-//         }
-//     }
-//     if(flag==0)
-//     {
-//         printf("Position invalid\n");
-//     }
-// }
 
-// void insertPosition() 
-// {
-//     int pos = 0, cnt = 0;
-//     int flag = 0;
-//     nd* prev = NULL; // Initialize prev to NULL
-//     printf("Enter the position at which to insert\n");
-//     scanf("%d", &pos);
-
-//     // Allocate memory for the new node
-//     nd* new_node = (nd*)malloc(sizeof(nd));
-//     if (new_node == NULL) 
-//     {
-//         printf("Memory allocation failed\n");
-//         return;
-//     }
-
-//     printf("Enter new node data\n");
-//     scanf("%d", &new_node->data);
-//     new_node->next = NULL; // Initialize the next pointer to NULL
-
-//     nd* temp = head;
-
-//     if (head == NULL || pos == 1) {
-//         // Insert at the beginning of the list
-//         new_node->next = head;
-//         head = new_node;
-//         flag = 1;
-//     } 
-//     else 
-//     {
-//         // Traverse the list to find the position to insert
-//         while (temp != NULL) {
-//             cnt++;
-//             if (pos != cnt) {
-//                 prev = temp;
-//                 temp = temp->next;
-//             } 
-//             else if (pos == cnt) {
-//                 // Insert at the specified position
-//                 new_node->next = temp;
-//                 prev->next = new_node;
-//                 flag = 1;
-//                 break; // Break out of the loop once inserted
-//             }
-//         }
-
-//         // If position is at the end of the list
-//         if (flag != 1 && prev != NULL && temp == NULL) {
-//             new_node->next = NULL;
-//             prev->next = new_node;
-//             flag = 1;
-//         }
-//     }
-
-//     if (flag == 0) {
-//         printf("Position invalid\n");
-//     }
-// }
-
-#include <stdio.h>
-#include <stdlib.h>
-
-
-
-void insertPosition() {
-    int pos = 0, cnt = 0;
-    int flag = 0;
-    nd* prev = NULL;
+void insertPosition()
+{
+    int pos,cnt=0;
     printf("Enter the position at which to insert\n");
-    scanf("%d", &pos);
-
-    if (pos < 1) {
-        printf("Invalid position. Position must be 1 or greater.\n");
-        return;
-    }
-
-    nd* new_node = (nd*)malloc(sizeof(nd));
-    if (new_node == NULL) {
-        printf("Memory allocation failed\n");
-        return;
-    }
-
-    printf("Enter new node data\n");
-    scanf("%d", &new_node->data);
-    new_node->next = NULL;
-
-    nd* temp = head;
-
-    if (head == NULL || pos == 1) {
-        new_node->next = head;
-        head = new_node;
-        flag=1;
-        return;
-    } 
-    else 
+    scanf("%d",&pos);
+    nd* new_node=(nd*)malloc(sizeof(nd));
+    if(head==NULL||pos==1)
     {
-        while (temp != NULL) 
+        printf("Enter new node data\n");
+        scanf("%d",&new_node->data);
+        new_node->next=head;
+        head=new_node;
+    }
+    else
+    {
+        nd* temp=head;
+        nd* prev_node=NULL;
+        while(temp!=NULL)
         {
             cnt++;
-            if (pos != cnt) 
+            if(cnt==pos)
             {
-                prev = temp;
-                temp = temp->next;
-            } 
-            else if (pos == cnt) 
-            {
-                new_node->next = temp;
-                prev->next = new_node;
-                flag = 1;
-                // break;
-                return;
+                printf("Enter new node data\n");
+                scanf("%d",&new_node->data);
+                new_node->next=temp->next;
+                prev_node->next=new_node;
+                prev_node=temp;
+                temp=temp->next;
+                break;
             }
+            prev_node=temp;
+            temp=temp->next;
         }
-
-        if (flag != 1 && prev != NULL && temp == NULL) {
-            new_node->next = NULL;
-            prev->next = new_node;
-            flag = 1;
-            return;
+        if(prev_node!=NULL&&temp==NULL&&pos==(cnt+1))
+        {
+            printf("Enter new node data\n");
+            scanf("%d",&new_node->data);
+            new_node->next=NULL;
+            prev_node->next=new_node;
+            prev_node=NULL;
         }
-}
-    if (flag == 0) 
-    {
-        printf("Invalid position. Position exceeds the size of the list.\n");
+        else
+        {
+            printf("Invalid position\n");
+        }
     }
 }
-
-// ... (rest of the code)
-
+void deleteWithKey()
+{
+    int key;
+    printf("Enter the key to be deleted\n");
+    scanf("%d",&key);
+    if(head==NULL)
+    {
+        printf("No elements in list\n");
+        return;
+    }
+    else if(head->data==key)
+    {
+        nd* ptr=head;
+        head=head->next;
+        free(ptr);
+        return;
+    }
+    else
+    {
+        nd* temp=head;
+        nd* prev;
+        while(temp!=NULL)
+        {
+            if(temp->data==key)
+            {
+                nd* ptr=temp;
+                prev->next=temp->next;
+                free(ptr);
+                return;
+            }
+            prev=temp;
+            temp=temp->next;
+        }
+        printf("No such key\n");
+    }
+}
+void deleteWithPosition()
+{
+    int pos,cnt=0;
+    printf("Enter the position at which to delete\n");
+    scanf("%d",&pos);
+    if(head==NULL)
+    {
+        printf("No element in list\n");
+        return;
+    }
+    else if(pos==1)
+    {
+        nd* ptr=head;
+        head=head->next;
+        free(ptr);
+        return;
+    }
+    else
+    {
+        nd* temp=head;
+        nd* prev=NULL;
+        nd* ptr;
+        while(temp!=NULL)
+        {
+            cnt++;
+            if(cnt==pos)
+            {
+                ptr=temp;
+                prev->next=temp->next;
+                free(ptr);
+                return;
+            }
+            prev=temp;
+            temp=temp->next;
+        }
+        printf("Position invalid\n");
+    }
+}
 void main()
 {
     printf("Do you wish to insert elements y or n\n");
@@ -333,18 +278,18 @@ void main()
             insertPosition();
             display();
             break;
-            // case 4:
-            // deleteWithKey();
-            // display();
-            // break;
-            // case 5:
-            // deleteWithPosition();
-            // display();
-            // break;
+            case 4:
+            deleteWithKey();
+            display();
+            break;
+            case 5:
+            deleteWithPosition();
+            display();
+            break;
             default:
             printf("Invalid choice\n");
         }
-        printf("Do you wish to insert elements y or n\n");
+        printf("Do you wish to perform other operations y or n\n");
         scanf(" %c",&decision);
     }
 }
